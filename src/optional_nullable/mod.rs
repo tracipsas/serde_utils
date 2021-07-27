0d47b80d598b67520c72085d3d0773bc8b62e84b
Thomas BAGREL <tbagrel@tracip.fr>
2021-07-27 16:18:47 +0200

    add optional_nullable

--- src/optional_nullable/mod.rs
+++ src/optional_nullable/mod.rs
@@ -15,7 +15,7 @@ where
     Ok(Some(Option::deserialize(deserializer)?))
 }
 
-pub fn serialize<T, S>(data: Option<Option<T>>, serializer: S) -> Result<S::Ok, S::Error>
+pub fn serialize<T, S>(data: &Option<Option<T>>, serializer: S) -> Result<S::Ok, S::Error>
 where
     T: Serialize,
     S: Serializer,
